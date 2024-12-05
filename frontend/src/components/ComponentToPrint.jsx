4e916a175971dc9f6af245b9461fffc350703132
skynetfrank <frank.uah@gmail.com>
2024-12-04 22:24:45 -0400

    refactor home 041224

--- frontend/src/components/ComponentToPrint.jsx
+++ frontend/src/components/ComponentToPrint.jsx
@@ -1,142 +1,123 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import dayjs from "dayjs";
-
 
 // Create Document Component
 // eslint-disable-next-line react/display-name
 const ComponentToPrint = React.forwardRef((props, ref) => {
-	const [razonSocial,setRazonsocial]=useState("")
-	const [direccion,setDireccion]=useState("")
-	const [rif,setRif]=useState("")
-	
-	const control = props.control;
-	const nfact = props.nfact;
-	const userInfo = props.userInfo;
-	const getPageMargins = () => {
-		const marginTop = '160px';
-		const marginRight = '75px';
-		const marginBottom = '0px';
-		const marginLeft = '70px';
-		return `@page { margin: ${marginTop} ${marginRight}  ${marginBottom} ${marginLeft}}`;
-	};
-	console.log("control:", control)
-	return (
-		<div ref={ref}>
-			<style>{getPageMargins()}</style>
-			<div>
-				<div>
-					<div>
-						<div className='encabezado-print'>
-							<div className='fact-paciente-info'>
-								<p>
-									<strong>Numero de R.I.F.: </strong>
-									{control.paciente.cedula}
-									<br />
-									<strong>Nombre o Razon Social: </strong>{' '}
-									{control.paciente.nombre}
-									<br />
-									<strong>Direccion Fiscal: </strong>
-									{control.paciente.direccion}
-									<br />
-									<strong>Telefonos: </strong>
-									{control.paciente.telefono}
-									<br />
-								</p>
-							</div>
-							<div>
-								<p>
-									<strong>Factura Numero:</strong> {nfact} <br />
-									<strong>Fecha de Emision:</strong>{' '}
-									{dayjs(new Date(props.control.createdAt)).format("DD/MM/YYYY")}
-									<br />
-									<strong>Vencimiento:</strong>{' '}
-									{dayjs(new Date(props.control.createdAt)).format("DD/MM/YYYY")}
-									<br />
-									<strong>Condiciones: </strong>
-									De Contado <br />
-								</p>
-							</div>
-						</div>
-						<div className='titulos-detalle-print'>
-							<p className='titulo-detalle-codigo'>Cantidad</p>
-							<p className='titulo-detalle-desc'>Descripcion</p>
-							<p className='titulo-detalle-precio'>Precio Unitario</p>
-							<p className='titulo-detalle-total'>Monto Bs.</p>
-						</div>
+  const control = props.control;
+  const nfact = props.nfact;
+  const nombre = props.razonSocial;
+  const rif = props.rif;
+  const direccion = props.direccion;
+  const telefono = props.telefono;
+  const userInfo = props.userInfo;
+  const getPageMargins = () => {
+    const marginTop = "160px";
+    const marginRight = "75px";
+    const marginBottom = "0px";
+    const marginLeft = "70px";
+    return `@page { margin: ${marginTop} ${marginRight}  ${marginBottom} ${marginLeft}}`;
+  };
+  console.log("props:", props);
+  return (
+    <div ref={ref}>
+      <style>{getPageMargins()}</style>
+      <div>
+        <div>
+          <div>
+            <div className="encabezado-print">
+              <div className="fact-paciente-info">
+                <p>
+                  <strong>Numero de R.I.F.: </strong>
+                  {rif ? rif : control.paciente.cedula}
+                  <br />
+                  <strong>Nombre o Razon Social: </strong> {nombre ? nombre : control.paciente.nombre}
+                  <br />
+                  <strong>Direccion Fiscal: </strong>
+                  {direccion ? direccion : control.paciente.direccion}
+                  <br />
+                  <strong>Telefonos: </strong>
+                  {telefono ? telefono : control.paciente.telefono}
+                  <br />
+                </p>
+              </div>
+              <div>
+                <p>
+                  <strong>Factura Numero:</strong> {nfact} <br />
+                  <strong>Fecha de Emision:</strong> {dayjs(new Date(props.control.createdAt)).format("DD/MM/YYYY")}
+                  <br />
+                  <strong>Vencimiento:</strong> {dayjs(new Date(props.control.createdAt)).format("DD/MM/YYYY")}
+                  <br />
+                  <strong>Condiciones: </strong>
+                  De Contado <br />
+                </p>
+              </div>
+            </div>
+            <div className="titulos-detalle-print">
+              <p className="titulo-detalle-codigo">Cantidad</p>
+              <p className="titulo-detalle-desc">Descripcion</p>
+              <p className="titulo-detalle-precio">Precio Unitario</p>
+              <p className="titulo-detalle-total">Monto Bs.</p>
+            </div>
 
+            <div className="content-servicios-print">
+              <ul>
+                {control.serviciosItems.map((item, inx) => (
+                  <li className="separator-print" key={inx}>
+                    <div className="row print">
+                      <div className="qty print">
+                        <p>{item.cantidad}</p>
+                      </div>
 
-						<div className='content-servicios-print'>
-							<ul>
-								{control.serviciosItems.map((item, inx) => (
-									<li className='separator-print' key={inx}>
-										<div className='row print'>
-											<div className='qty print'>
-												<p>{item.cantidad}</p>
-											</div>
+                      <div className="descripcion-print">
+                        <span className="nombre-marca-print">{item.servicio.nombre}</span>
+                        <p id="mini-descripcion">Atendido por: Dr. {control.doctor.nombre}</p>
+                      </div>
 
-											<div className='descripcion-print'>
-												<span className='nombre-marca-print'>
-													{item.servicio.nombre}
-												</span>
-												<p id='mini-descripcion'>
-													Atendido por: Dr. {control.doctor.nombre}
-												</p>
-											</div>
+                      <div className="qty-price print">
+                        <p>{(item.precioServ * control.cambioBcv).toFixed(2)}</p>
+                      </div>
+                      <div className="qty-price total">
+                        <p>{(item.cantidad * item.precioServ * control.cambioBcv).toFixed(2)}</p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div>
+            <br></br>
+          </div>
+          <div className="bottom-resumen-print">
+            <div className="flx jsb">
+              <span className="txt-align-l">Base Imponible</span>
+              <span className="txt-align-r">Bs. {control.montoBs.toFixed(2)}</span>
+            </div>
+            <hr />
+            <div className="flx jsb">
+              <span className="txt-align-l">Monto Exento:</span>
+              <span className="txt-align-r">Bs. {0.0}</span>
+            </div>
 
-											<div className='qty-price print'>
-												<p>
-													{(item.precioServ * control.cambioBcv).toFixed(2)}
-												</p>
-											</div>
-											<div className='qty-price total'>
-												<p>
-													{(
-														item.cantidad *
-														item.precioServ *
-														control.cambioBcv
-													).toFixed(2)}
-												</p>
-											</div>
-										</div>
-									</li>
-								))}
-							</ul>
-						</div>
-					</div>
-					<div>
-						<br></br>
-					</div>
-					<div className='bottom-resumen-print'>
-						<div className='flx jsb'>
-							<span className='txt-align-l'>Base Imponible</span>
-							<span className='txt-align-r'>Bs. {control.montoBs.toFixed(2)}</span>
-						</div>
-						<hr />
-						<div className='flx jsb'>
-							<span className='txt-align-l'>Monto Exento:</span>
-							<span className='txt-align-r'>Bs. {0.0}</span>
-						</div>
+            <hr />
 
-						<hr />
+            <div className="flx jsb">
+              <span className="txt-align-l">I.V.A. 16%</span>
+              <span className="txt-align-r">Bs.{control.montoIva?.toFixed(2)}</span>
+            </div>
 
-						<div className='flx jsb'>
-							<span className='txt-align-l'>I.V.A. 16%</span>
-							<span className='txt-align-r'>Bs.{control.montoIva?.toFixed(2)}</span>
-						</div>
+            <hr />
 
-						<hr />
-
-						<div className='flx jsb'>
-							<span className='txt-align-l'>Total</span>
-							<span className='txt-align-r'>Bs.{Number(control.montoBs+control.montoIva).toFixed(2)}</span>
-						</div>
-
-
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="flx jsb">
+              <span className="txt-align-l">Total</span>
+              <span className="txt-align-r">Bs.{Number(control.montoBs + control.montoIva).toFixed(2)}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 });
 
 export default ComponentToPrint;
--- frontend/src/screens/PrintFacturaScreen.jsx
+++ frontend/src/screens/PrintFacturaScreen.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { detailsControl } from '../actions/controlActions';
-import ComponentToPrint from '../components/ComponentToPrint';
-import { useReactToPrint } from 'react-to-print';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPrint } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { detailsControl } from "../actions/controlActions";
+import ComponentToPrint from "../components/ComponentToPrint";
+import { useReactToPrint } from "react-to-print";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPrint } from "@fortawesome/free-solid-svg-icons";
 
 export default function PrintFacturaScreen(props) {
   const contentRef = useRef(null);
@@ -15,11 +15,15 @@ export default function PrintFacturaScreen(props) {
 
   const params = useParams();
   const { id: controlId } = params;
-  const [nfact, setNfact] = useState('');
+  const [nfact, setNfact] = useState("");
+  const [razonSocial, setRazonSocial] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [rif, setRif] = useState("");
+  const [telefono, setTelefono] = useState("");
 
-  const controlDetails = useSelector(state => state.controlDetails);
+  const controlDetails = useSelector((state) => state.controlDetails);
   const { control, loading, error } = controlDetails;
-  const userSignin = useSelector(state => state.userSignin);
+  const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
 
   const dispatch = useDispatch();
@@ -27,6 +31,12 @@ export default function PrintFacturaScreen(props) {
   useEffect(() => {
     if (!control || (control && control._id !== controlId)) {
       dispatch(detailsControl(controlId));
+    }
+    if (control) {
+      setRazonSocial(control.nombre);
+      setRif(control.cedula);
+      setDireccion(control.direccion);
+      setTelefono(control.telefono);
     }
   }, [dispatch, controlId, control]);
 
@@ -45,7 +55,7 @@ export default function PrintFacturaScreen(props) {
             placeholder=" "
             value={nfact}
             required
-            onChange={e => setNfact(e.target.value)}
+            onChange={(e) => setNfact(e.target.value)}
           />
           <label htmlFor="cedula" className="user-label">
             Factura No.
@@ -57,12 +67,26 @@ export default function PrintFacturaScreen(props) {
             className="input small"
             autoComplete="off"
             placeholder=" "
-            value={nfact}
+            value={rif}
             required
-            onChange={e => setNfact(e.target.value)}
+            onChange={(e) => setRif(e.target.value)}
           />
           <label htmlFor="cedula" className="user-label">
-            Factura No.
+            R.I.F.
+          </label>
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            className="input medium"
+            autoComplete="off"
+            placeholder=" "
+            value={razonSocial}
+            required
+            onChange={(e) => setRazonSocial(e.target.value)}
+          />
+          <label htmlFor="cedula" className="user-label">
+            Razon Social
           </label>
         </div>
         <div className="input-group">
@@ -71,19 +95,42 @@ export default function PrintFacturaScreen(props) {
             className="input small"
             autoComplete="off"
             placeholder=" "
-            value={nfact}
+            value={telefono}
             required
-            onChange={e => setNfact(e.target.value)}
+            onChange={(e) => setTelefono(e.target.value)}
           />
           <label htmlFor="cedula" className="user-label">
-            Factura No.
+            R.I.F.
+          </label>
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            className="input large"
+            autoComplete="off"
+            placeholder=" "
+            value={direccion}
+            required
+            onChange={(e) => setDireccion(e.target.value)}
+          />
+          <label htmlFor="cedula" className="user-label">
+            Direccion Fiscal
           </label>
         </div>
         <button className="btn-print" onClick={handlePrint}>
           <FontAwesomeIcon icon={faPrint} />
         </button>
       </div>
-      <ComponentToPrint control={control} nfact={nfact} userInfo={userInfo} ref={contentRef} />
+      <ComponentToPrint
+        control={control}
+        nfact={nfact}
+        rif={rif}
+        direccion={direccion}
+        razonSocial={razonSocial}
+        telefono={telefono}
+        userInfo={userInfo}
+        ref={contentRef}
+      />
     </div>
   );
 }
