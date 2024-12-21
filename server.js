83fcb2e0a6d138d3ad6ed46ad6276a3a2b9fcf5b
Franklin Bolivar <frank.uah@gmail.com>
2024-12-21 14:26:18 -0400

    refactor dist for build directory error

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use("/api/gastos", gastoRouter);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/dist/index.html')));
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
