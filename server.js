e395126ea85b7073132d18ab1bfcc333bcf9734b
skynetfrank <frank.uah@gmail.com>
2024-12-21 23:16:17 -0400

    refactor url db

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import path from 'path';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import pacienteRouter from './routers/pacienteRouter.js';
-import doctorRouter from './routers/doctorRouter.js';
-import controlRouter from './routers/controlRouter.js';
-import servicioRouter from './routers/servicioRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import pacienteRouter from "./routers/pacienteRouter.js";
+import doctorRouter from "./routers/doctorRouter.js";
+import controlRouter from "./routers/controlRouter.js";
+import servicioRouter from "./routers/servicioRouter.js";
 import gastoRouter from "./routers/gastoRouter.js";
 
 dotenv.config();
@@ -15,25 +15,25 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const localdb = 'mongodb://localhost/plazadentaldb';
+const localdb = "mongodb://localhost/plazadentaldb";
 //process.env.MONGODB_URI;
 mongoose
-  .connect(localdb)
-  .then(() => console.log('mongodb =>: conectado'))
-  .catch(e => console.log(e.message));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("mongodb =>: conectado"))
+  .catch((e) => console.log(e.message));
 
-app.use('/api/users', userRouter);
-app.use('/api/productos', productRouter);
-app.use('/api/pacientes', pacienteRouter);
-app.use('/api/doctores', doctorRouter);
-app.use('/api/controles', controlRouter);
-app.use('/api/servicios', servicioRouter);
+app.use("/api/users", userRouter);
+app.use("/api/productos", productRouter);
+app.use("/api/pacientes", pacienteRouter);
+app.use("/api/doctores", doctorRouter);
+app.use("/api/controles", controlRouter);
+app.use("/api/servicios", servicioRouter);
 app.use("/api/gastos", gastoRouter);
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/dist/index.html')));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/frontend/dist/index.html")));
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
@@ -42,5 +42,5 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('servidor ok escuchando en http://localhost');
+  console.log("servidor ok escuchando en http://localhost");
 });
