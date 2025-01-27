d33c6379f72bf588578b670fad969b886cb2b97c
Franklin Bolivar <frank.uah@gmail.com>
2025-01-27 14:32:42 -0400

    refactor 270125 232pm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -99,7 +99,7 @@ function App() {
             <Outlet />
           </main>
           <footer>
-            <span className="negrita">24ENERO2025</span>
+            <span className="negrita">27ENERO2025</span>
           </footer>
         </div>
       )}
